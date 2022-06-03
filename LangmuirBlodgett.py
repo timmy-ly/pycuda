b89fd835c622deed66eeb5f267debb209e29aeee
Timmy Ly <timmy.ly@wwu.de>
2022-06-03 15:52:50 +0200

    edge case vamplitude is not provided as argument

--- LangmuirBlodgett.py
+++ LangmuirBlodgett.py
@@ -173,8 +173,8 @@ class LBCont(Continuation):
   def __init__(self, ParamsCMDArgs, ParamsOther):
     super().__init__(ParamsCMDArgs, ParamsOther)
   def get_Subdir(self, args):
-    if(args["vamplitude"]==0):
-      SubdirSuffix = ("Lx{Lx:g}_Ly{Ly:g}_Nx{Nx:d}_Ny{Ny:d}_eps{epsilontol:g}_noise{noise:g}_ys{ys:g}_ls{ls:g}_c0{c0:g}_v{v:g}".format(**args))
-    else:
+    if(("vamplitude" in args) and args["vamplitude"]!=0):
       SubdirSuffix = ("Lx{Lx:g}_Ly{Ly:g}_Nx{Nx:d}_Ny{Ny:d}_eps{epsilontol:g}_noise{noise:g}_ys{ys:g}_ls{ls:g}_c0{c0:g}_v{v:g}_A{vamplitude:g}_w{vperiod:g}".format(**args))
+    else:
+      SubdirSuffix = ("Lx{Lx:g}_Ly{Ly:g}_Nx{Nx:d}_Ny{Ny:d}_eps{epsilontol:g}_noise{noise:g}_ys{ys:g}_ls{ls:g}_c0{c0:g}_v{v:g}".format(**args))
     return self.SubdirPrefix + "_" + SubdirSuffix
